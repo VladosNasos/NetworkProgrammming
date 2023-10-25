d26b8fab0dfc2d7e4ea70929644b0a779b62b9ab
VladosNasos <bezzzubkovlad@gmail.com>
2023-10-26 02:35:14 +0300

    Add project files.

---
+++ NetworkProgram/ChatMessage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkProgram
+{
+    internal class ChatMessage
+    {
+        public string Login { get; set; } = null!;
+        public string Text { get; set; } = null!;
+        public DateTime Moment { get; set; }  // дата отправки
+
+        public override string ToString()
+        {
+            return $"{Login}: {Text}";
+        }
+
+        public string GetSendTime()
+        {
+            DateTime now = DateTime.Now;
+            TimeSpan date = now - Moment;
+            string sendTime = "";
+            if (now.Date > Moment.Date)  // если прошло больше суток
+            {
+                sendTime = date.ToString("dd") + " д. назад";
+            }
+            else if (now.Date == Moment.Date)  // если это сегодняшний день
+            {
+                if (date.TotalSeconds >= 60)
+                {
+                    sendTime = date.ToString("mm") + " мин. назад";
+                }
+                else if (date.TotalMinutes >= 60)
+                {
+                    sendTime = date.ToString("HH") + " ч. назад";
+                }
+                else
+                {
+                    sendTime = date.ToString("ss") + " сек. назад";
+                }
+            }
+            return sendTime;
+        }
+    }
+}
